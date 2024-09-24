ebd4fcc3e2bd754b517c2f06b638d40f6a70680f
FreshMag <pescomagnani@gmail.com>
2024-09-24 18:08:05 +0200

    style(chemistLab7): removed blank space

--- chemistLab7/src/main/scala/u07/simulation/utils/SPNUtils.scala
+++ chemistLab7/src/main/scala/u07/simulation/utils/SPNUtils.scala
@@ -2,7 +2,7 @@ package u07.simulation.utils
 
 object SPNUtils:
   export u04.mvc.MonadicMVC.*
-  
+
   def eventStateToChartValues[T](eventStateMap: Map[T, Int], filterPlaces: Set[T]): Map[String, Double] =
     eventStateMap
       .filter((place, _) => !filterPlaces.contains(place))
